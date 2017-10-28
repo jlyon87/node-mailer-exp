382ee317692da1221d660e928d951ba7338eea55
jlyonas <jlyon@acumensolutions.com>
2017-10-28 09:38:10 -0400

    remove createTestAccount

--- server.js
+++ server.js
@@ -2,31 +2,29 @@ const nodemailer = require("nodemailer");
 const gmail = require("./gmail.js");
 const contacts = require("./contacts.js");
 
-nodemailer.createTestAccount((err, account) => {
-	let transporter = nodemailer.createTransport({
-		host: gmail.smtp,
-		port: 587,
-		secure: false,
-		auth: {
-			user: gmail.username,
-			pass: gmail.password,
-		},
-	});
+let transporter = nodemailer.createTransport({
+	host: gmail.smtp,
+	port: 587,
+	secure: false,
+	auth: {
+		user: gmail.username,
+		pass: gmail.password,
+	},
+});
 
-	let mailOptions = {
-		from: gmail.username,
-		to: contacts.justin,
-		subject: "Hello ✔",
-		text: "Hello World, from Node JS!",
-		html: "<strong>Hello World, from Node JS!</strong>",
-	};
+let mailOptions = {
+	from: gmail.username,
+	to: contacts.justin,
+	subject: "Hello ✔",
+	text: "Hello World, from Node JS!",
+	html: "<strong>Hello World, from Node JS!</strong>",
+};
 
-	transporter.sendMail(mailOptions, (err, info) => {
-		if(err) {
-			throw err;
-		}
+transporter.sendMail(mailOptions, (err, info) => {
+	if(err) {
+		throw err;
+	}
 
-		console.log("Message send: %s", info.messageId);
-		console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-	});
+	console.log("Message send: %s", info.messageId);
+	console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 });
